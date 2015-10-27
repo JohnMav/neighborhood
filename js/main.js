0fcf1b80a10dcd2d6bc36149b4fe06a2a1155e6e
kennyklee <kennyklee@gmail.com>
2015-10-27 12:19:52 -0700

    Broken code, but still working through it

--- js/main.js
+++ js/main.js
@@ -23,47 +23,68 @@ var locations = [
     }
 ];
 
-var GoogleMarker = function(data) {
-    this.title = ko.observable(data.title);
-    this.lat = ko.observable(data.lat);
-    this.lng = ko.observable(data.lng);
-};
+//Global variables
+var map;
+var marker;
+var markers = [];
+var mapElement = document.getElementById('map');
 
-
-//Default location where the map is initialized & centered.  NOT KNOCKUTJS
+//Default location where the map is initialized & centered.
 var defaultLoc = ko.observable( {lat: locations[0].lat, lng: locations[0].lng, title: 'Default Boring'});
 
-var currentMarker = ko.observable({
-    //Calling defaultLoc from model
-    position: {
-        lat: defaultLoc().lat,
-        lng: defaultLoc().lng
-    },
-    title: defaultLoc().title
-});
-
+//Map options
 var mapOptions = {
     center: defaultLoc(),
     zoom: 12
 };
 
-//  GOOGLE MAP
+//Create map using Google api.
+map = new google.maps.Map(mapElement, mapOptions);
+
+var marker2Observable = function(data) {
+    this.title = ko.observable(data.title);
+    this.lat = ko.observable(data.lat);
+    this.lng = ko.observable(data.lng);
+};
+
+var marker2Google = function(data) {
+    return {
+        positions: {
+             lat: data().lat,
+             lng: data().lng
+         },
+         title: data().title
+    }
+};
+
+console.log("currentMarker is " + marker2Google(defaultLoc).title);
+
+//var currentMarker = ko.computed(function() {
+//    return {
+//         positions: {
+//             lat: defaultLoc().lat,
+//             lng: defaultLoc().lng
+//         },
+//         title: defaultLoc().title
+//    }
+//    }, this);
+
+
+//  VIEW-MODEL
 // ==================================================
+
 var initMap = function(pin) {
-    var map;
-    var marker;
-    var mapElement = document.getElementById('map');
-
-    //Create map using Google api.
-    map = new google.maps.Map(mapElement, mapOptions);
-
-
+    console.log("initMap executed");
     // Default Marker
     //==================================================
 
+    //Default location where the map is initialized & centered.  NOT KNOCKUTJS
+    //var defaultLoc = ko.observable( {lat: locations[0].lat, lng: locations[0].lng, title: 'Default Boring'});
 
-    marker = ko.observable(new google.maps.Marker(pin));
-    marker().setMap(map);
+    //marker = new google.maps.Marker(defaultLoc());
+    addMarker(pin);
+    showMarkers();
+
 
     // All Markers in Model
     //==================================================
@@ -97,10 +118,40 @@ var initMap = function(pin) {
     //marker.setMap(map);
 };
 
-//  VIEW-MODEL
-// ==================================================
+// Adds a marker to the map and push to the array.
+function addMarker(location) {
+    var marker = new google.maps.Marker(location);
+    markers.push(marker);
+}
+
+// Sets the map on all markers in the array.
+function setMapOnAll(map) {
+    for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(map);
+       console.log(markers[i]);
+    }
+    console.log("==============");
+}
+
+// Removes the markers from the map, but keeps them in the array.
+function clearMarkers() {
+    setMapOnAll(null);
+}
+
+// Shows any markers currently in the array.
+function showMarkers() {
+    setMapOnAll(map);
+}
+
+// Deletes all markers in the array by removing references to them.
+function deleteMarkers() {
+    clearMarkers();
+    markers = [];
+}
 
 var ViewModel = function () {
+
+    console.log("ViewModel executed");
     var self = this; // Using self to access outer functions.
 
     // Get Locations
@@ -109,13 +160,21 @@ var ViewModel = function () {
     //Set current location.
     this.locList = ko.observableArray([]); //Initialize array of locations
 
-    this.currentLoc = ko.observable(self.locList()[0]);
-
     // Take the locations model, filter it through GoogleMarker function (make each property observable),
     // and add to the array of locations
     locations.forEach(function (loc) {
-        self.locList.push(new GoogleMarker(loc));
+        self.locList.push(new marker2Observable(loc));
+        defaultLoc(loc);
+        console.log(defaultLoc());
+
+        //console.log(marker2Observable(loc));
+        addMarker(defaultLoc());
     });
+
+
+    this.currentLoc = ko.observable(self.locList()[0]);
+
+    console.log("currentLoc is:" + this.currentLoc().title);
 
     // FILTER RESULTS
     // http://www.knockmeout.net/2011/04/utility-functions-in-knockoutjs.html
@@ -143,28 +202,33 @@ var ViewModel = function () {
         self.currentLoc(clickedLoc);
         //currentMarker(clickedLoc);
 
-        this.updateMarker = ko.observable({
+        var updateMarker = ko.observable({
             //Calling defaultLoc from model
             position: {
-                lat: self.currentLoc().lat(),
-                lng: self.currentLoc().lng()
+                lat: self.currentLoc().lat,
+                lng: self.currentLoc().lng
             },
-            title: self.currentLoc().title()
+            title: self.currentLoc().title
         });
 
-        initMap(this.updateMarker());
+        console.log("Markers deleted");
+        console.log("clickedLoc = " + clickedLoc);
+        addMarker(clickedLoc);
+        showMarkers();
+
+        //Really janky way of updating the markers based on clicks.
+        //initMap(this.updateMarker());
 
         //Debug
-        console.log("Clicked Title = " + clickedLoc.title());
-        console.log("Clicked Lat = " + clickedLoc.lat());
-        console.log("Clicked Lng = " + clickedLoc.lng());
-        console.log("Current Title = " + self.currentLoc().title());
-        console.log("Current Lat = " + self.currentLoc().lat());
-        console.log("Current Lng = " + self.currentLoc().lng());
+        //console.log("Clicked Title = " + clickedLoc.title());
+        //console.log("Clicked Lat = " + clickedLoc.lat());
+        //console.log("Clicked Lng = " + clickedLoc.lng());
+        //console.log("Current Title = " + self.currentLoc().title());
+        //console.log("Current Lat = " + self.currentLoc().lat());
+        //console.log("Current Lng = " + self.currentLoc().lng());
     };
+    console.log("Viewmodel ended");
 };
 
-// Capture clicks below.  TODO: This can be moved to a 'Update' object.
-
 ko.applyBindings(new ViewModel);  //I forgot why this is needed vs just "ViewModel.init()";
-google.maps.event.addDomListener(window, 'load', initMap(currentMarker()));
+google.maps.event.addDomListener(window, 'load', initMap(marker2Google(defaultLoc)));
