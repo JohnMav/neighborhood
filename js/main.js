e5f14f6a9b745a5a9a3365a7b178fa5b061a5e41
kennyklee <kennyklee@gmail.com>
2015-10-26 22:16:31 -0700

    Click on list updates the map, but super sucks.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-
-
 //  MODEL
 // ==================================================
 var locations = [
@@ -25,17 +23,33 @@ var locations = [
     }
 ];
 
-//Default location where the map is initialized & centered.
-var defaultLoc = {lat: locations[0].lat, lng: locations[0].lng, title: "Default & Boring"};
+var GoogleMarker = function(data) {
+    this.title = ko.observable(data.title);
+    this.lat = ko.observable(data.lat);
+    this.lng = ko.observable(data.lng);
+};
+
+
+//Default location where the map is initialized & centered.  NOT KNOCKUTJS
+var defaultLoc = ko.observable( {lat: locations[0].lat, lng: locations[0].lng, title: 'Default Boring'});
+
+var currentMarker = ko.observable({
+    //Calling defaultLoc from model
+    position: {
+        lat: defaultLoc().lat,
+        lng: defaultLoc().lng
+    },
+    title: defaultLoc().title
+});
 
 var mapOptions = {
-    center: defaultLoc,
+    center: defaultLoc(),
     zoom: 12
 };
 
 //  GOOGLE MAP
 // ==================================================
-var initMap = function() {
+var initMap = function(pin) {
     var map;
     var marker;
     var mapElement = document.getElementById('map');
@@ -46,18 +60,10 @@ var initMap = function() {
 
     // Default Marker
     //==================================================
-    var markerDefault = {
 
-        //Calling defaultLoc from model
-        position: {
-            lat: defaultLoc.lat,
-            lng: defaultLoc.lng
-        },
-        title: defaultLoc.title
-    };
 
-    marker = new google.maps.Marker(markerDefault);
-    marker.setMap(map);
+    marker = ko.observable(new google.maps.Marker(pin));
+    marker().setMap(map);
 
     // All Markers in Model
     //==================================================
@@ -76,12 +82,13 @@ var initMap = function() {
 
     // Clicked Marker
     //==================================================
+    //console.log(ViewModel.c);
     //var markerCurrent = {
     //
     //    //Calling defaultLoc from model
     //    position: {
-    //        lat: ViewModel.init().currentLoc().lat(),
-    //        lng: ViewModel.init().currentLoc().lng()
+    //        lat: ViewModel.currentLoc().lat(),
+    //        lng: ViewModel.currentLoc().lng()
     //    },
     //    title: ViewModel.init().currentLoc().title()
     //};
@@ -92,65 +99,72 @@ var initMap = function() {
 
 //  VIEW-MODEL
 // ==================================================
-var GoogleMarker = function(data) {
-    this.title = ko.observable(data.title);
-    this.lat = ko.observable(data.lat);
-    this.lng = ko.observable(data.lng);
-};
 
-var ViewModel = {
-    init: function () {
-        var self = this; // Using self to access outer functions.
+var ViewModel = function () {
+    var self = this; // Using self to access outer functions.
 
-        this.locList = ko.observableArray([]); //Initialize array of locations
+    // Get Locations
+    //==================================================
 
-        // Take the locations model, filter it through GoogleMarker function (make each property observable),
-        // and add to the array of locations
-        locations.forEach(function (loc) {
-            self.locList.push(new GoogleMarker(loc));
+    //Set current location.
+    this.locList = ko.observableArray([]); //Initialize array of locations
+
+    this.currentLoc = ko.observable(self.locList()[0]);
+
+    // Take the locations model, filter it through GoogleMarker function (make each property observable),
+    // and add to the array of locations
+    locations.forEach(function (loc) {
+        self.locList.push(new GoogleMarker(loc));
+    });
+
+    // FILTER RESULTS
+    // http://www.knockmeout.net/2011/04/utility-functions-in-knockoutjs.html
+    // ==================================================
+
+    // Create utility function TODO: Move into Utility section?
+    var stringStartsWith = function (string, startsWith) {
+        string = string || "";
+        if (startsWith.length > string.length)
+            return false;
+        return string.substring(0, startsWith.length) === startsWith;
+    };
+
+    this.listSearch = ko.observable("");
+
+    this.filteredRecords = ko.computed(function() {
+        var filter = self.listSearch().toLowerCase();
+        return ko.utils.arrayFilter(self.locList(), function(item){
+            return (self.listSearch().length == 0 || stringStartsWith(item.title().toLowerCase(), filter));
+        });
+    });
+
+    //With a click, set the current location, and update the currentLoc.
+    this.setLoc = function (clickedLoc) {
+        self.currentLoc(clickedLoc);
+        //currentMarker(clickedLoc);
+
+        this.updateMarker = ko.observable({
+            //Calling defaultLoc from model
+            position: {
+                lat: self.currentLoc().lat(),
+                lng: self.currentLoc().lng()
+            },
+            title: self.currentLoc().title()
         });
 
-        // Set the default current location to the 1st one.
-        this.currentLoc = ko.observable(this.locList()[0]);
+        initMap(this.updateMarker());
 
-        // FILTER RESULTS
-        // http://www.knockmeout.net/2011/04/utility-functions-in-knockoutjs.html
-        // ==================================================
-
-        // Create utility function TODO: Move into Utility section?
-        var stringStartsWith = function (string, startsWith) {
-            string = string || "";
-            if (startsWith.length > string.length)
-                return false;
-            return string.substring(0, startsWith.length) === startsWith;
-        };
-
-        this.listSearch = ko.observable("");
-
-        this.filteredRecords = ko.computed(function() {
-            var filter = self.listSearch().toLowerCase();
-            return ko.utils.arrayFilter(self.locList(), function(item){
-                return (self.listSearch().length == 0 || stringStartsWith(item.title().toLowerCase(), filter));
-            });
-        });
-
-        // Capture clicks below.  TODO: This can be moved to a 'Update' object.
-        this.setLoc = function (clickedLoc) {
-            self.currentLoc(clickedLoc);
-
-
-
-
-            //Debug
-            console.log("Clicked Title = " + clickedLoc.title());
-            console.log("Clicked Lat = " + clickedLoc.lat());
-            console.log("Clicked Lng = " + clickedLoc.lng());
-            console.log("Current Title = " + self.currentLoc().title());
-            console.log("Current Lat = " + self.currentLoc().lat());
-            console.log("Current Lng = " + self.currentLoc().lng());
-        };
-    }
+        //Debug
+        console.log("Clicked Title = " + clickedLoc.title());
+        console.log("Clicked Lat = " + clickedLoc.lat());
+        console.log("Clicked Lng = " + clickedLoc.lng());
+        console.log("Current Title = " + self.currentLoc().title());
+        console.log("Current Lat = " + self.currentLoc().lat());
+        console.log("Current Lng = " + self.currentLoc().lng());
+    };
 };
 
-google.maps.event.addDomListener(window, 'load', initMap);
-ko.applyBindings(new ViewModel.init());  //I forgot why this is needed vs just "ViewModel.init()";
+// Capture clicks below.  TODO: This can be moved to a 'Update' object.
+
+ko.applyBindings(new ViewModel);  //I forgot why this is needed vs just "ViewModel.init()";
+google.maps.event.addDomListener(window, 'load', initMap(currentMarker()));
